55b19eea7aaef812423f4d2be2b22011d516af4b
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2021-09-19 00:38:17 -0300

    Componentes com Typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -5,8 +5,14 @@ import "../styles/repositories.scss";
 
 // https://api.github.com/orgs/rocketseat/repos
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
@@ -18,8 +24,10 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        {repositories.map((repository, index) => {
-          return <RepositoryItem key={index} repository={repository} />;
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
         })}
       </ul>
     </section>
